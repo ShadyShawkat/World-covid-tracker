a811dc5cbe68b9425e2dacdbc13dda2abc785272
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-02 01:31:14 +0200

    Add routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
 
 import { fetchCountries } from './redux/covidSlice';
+
+import Nav from './components/Nav/Nav';
+import CountryList from './components/CountryList/CountryList';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -9,7 +13,16 @@ const App = () => {
     dispatch(fetchCountries());
   }, [dispatch]);
 
-  return <div>App is working</div>;
+  return (
+    <>
+      <Nav />
+      <div className="mt-16 px-12 pt-6">
+        <Routes>
+          <Route path="/" element={<CountryList />} />
+        </Routes>
+      </div>
+    </>
+  );
 };
 
 export default App;
