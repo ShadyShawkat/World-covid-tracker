494b3c71274493e83f606e25455981a419932303
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-02 03:13:03 +0200

    Add method to fetch continents data

--- src/API/APIHandler.js
+++ src/API/APIHandler.js
@@ -1,9 +1,16 @@
-const apiURL = 'https://api.covid19tracking.narrativa.com/api?date_from=2022-01-31';
+const apiContinentsURL = 'https://corona.lmao.ninja/v2/continents';
+const apiCountriesURL = 'https://corona.lmao.ninja/v2/countries';
+
 // const apiCountryURL = (country) => `https://api.covid19tracking.narrativa.com/api/country/${country}?date_from=2022-01-31`;
 
 const fetchAllCountries = async () => {
-  const response = await fetch(apiURL).then((data) => data.json());
+  const response = await fetch(apiCountriesURL).then((data) => data.json());
   return response;
 };
 
-export default { fetchAllCountries };
+const fetchAllContinents = async () => {
+  const response = await fetch(apiContinentsURL).then((data) => data.json());
+  return response;
+};
+
+export default { fetchAllCountries, fetchAllContinents };
