6967ea0cadfcd1c82226d303f3dc622e3c8507f7
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-02 20:02:48 +0200

    Add functionality to scroll to top on route change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 
 import { fetchCountries, fetchTotalData } from './redux/covidSlice';
 
 import Nav from './components/Nav/Nav';
 import Home from './components/Home/Home';
+import ContinentDetails from './components/ContinentDetails/ContinentDetails';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -14,12 +15,25 @@ const App = () => {
     dispatch(fetchTotalData());
   }, [dispatch]);
 
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }, [location]);
+
   return (
     <>
       <Nav />
       <div className="mt-16 px-12 pt-6">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route
+            path="/continent/:continentName"
+            element={<ContinentDetails />}
+          />
         </Routes>
       </div>
     </>
