e223596e9ed3c56434f2e58abdcc1f39bc14c0aa
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-02 03:13:35 +0200

    Update CountryList.js component

--- src/components/CountryList/CountryList.js
+++ src/components/CountryList/CountryList.js
@@ -1,26 +1,18 @@
-import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 import styles from './CountryList.module.css';
 
-import Spinner from '../Spinner/Spinner';
 import Country from '../Country/Country';
 
-const CountryList = () => {
-  const { countries } = useSelector((state) => state.covid);
+const CountryList = ({ countries }) => (
+  <ul className="grid grid-cols-2 rounded-lg overflow-hidden gap-0.5">
+    {countries.map((country, index) => (
+      <li key={country.country} className={styles['country-card']}>
+        <Country country={country} index={index} />
+      </li>
+    ))}
+  </ul>
+);
 
-  return (
-    <>
-      {!countries.length && <Spinner />}
-      {countries.length > 0 && (
-        <ul className="grid grid-cols-2">
-          {countries.map((country) => (
-            <li key={country.country} className={styles['country-card']}>
-              <Country country={country} />
-            </li>
-          ))}
-        </ul>
-      )}
-    </>
-  );
-};
+CountryList.propTypes = PropTypes.array.isRequired;
 
 export default CountryList;
