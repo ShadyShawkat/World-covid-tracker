8f3dcb89e61485a91f8093a6700a58f87c320211
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-02 19:59:58 +0200

    Adjust responsivness and add button

--- src/components/Continent/Continent.js
+++ src/components/Continent/Continent.js
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
 import images from '../../images';
 
 const Continent = ({ continent: { continent, cases } }) => (
   <Link
     to={`/continent/${continent}`}
-    className="md:flex-row flex flex-col px-3 py-10 hover:bg-gray-600 justify-between group h-full"
+    className="md:flex-row flex flex-col px-3 py-10 hover:bg-gray-600 justify-between group h-full relative"
   >
+    <BsFillArrowUpRightCircleFill className="text-blue-800 right-2 top-2 absolute text-lg group-hover:text-white" />
     <img
       src={images[continent]}
       alt="continent map"
