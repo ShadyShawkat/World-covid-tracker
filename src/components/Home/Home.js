fd938036b8ff0199305d95593e06aa5ce2f1c77d
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-02 21:02:31 +0200

    Remove unwanted code

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,47 +4,14 @@ import { FcGlobe } from 'react-icons/fc';
 
 import Spinner from '../Spinner/Spinner';
 import ContinentList from '../ContinentList/ContinentList';
-// import CountryList from '../CountryList/CountryList';
 
 const Home = () => {
-  // const {
-  //   countries,
-  //   totalData: { totalCases, totalDeaths },
-  // } = useSelector((state) => state.covid);
   const {
     continents,
     totalData: { totalCases, totalDeaths },
   } = useSelector((state) => state.covid);
 
   return (
-    // <>
-    //   {!countries.length && <Spinner />}
-    //   {countries.length > 0 && (
-    //     <>
-    //       <div className="flex gap-3 items-center w-full justify-center
-    //                      pb-16 pt-6 bg-blue-dark rounded-t-lg">
-    //         <FcGlobe className="text-8xl bg-white rounded-full" />
-    //         <div className="flex flex-col font-bold text-2xl text-white">
-    //           <CountUp
-    //             delay={1}
-    //             end={totalCases}
-    //             separator=","
-    //             prefix="Total Cases: "
-    //             duration={3}
-    //           />
-    //           <CountUp
-    //             delay={1}
-    //             prefix="Total Deaths: "
-    //             end={totalDeaths}
-    //             separator=","
-    //             duration={3}
-    //           />
-    //         </div>
-    //       </div>
-    //       <CountryList countries={countries} />
-    //     </>
-    //   )}
-    // </>
     <>
       {!continents.length && <Spinner />}
       {continents.length > 0 && (
