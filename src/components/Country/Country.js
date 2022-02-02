32a11f88e06a0a09a7d6e3931270c4aa285b7d3c
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-02 19:59:30 +0200

    Remove continent txt from country card and add btn

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,21 +1,22 @@
-/* eslint-disable object-curly-newline */
+/* eslint-disable object-curly-newline, implicit-arrow-linebreak */
 import PropTypes from 'prop-types';
+import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
 
 const Country = ({
   country: {
     country,
-    continent,
     cases,
     countryInfo: { flag },
   },
   index,
 }) => {
-  const bgColor = (fade = 'ff') => `#${
-    (index + 1) % 4 === 2 || (index + 1) % 4 === 3 ? '7c9edf' : '708dc2'
-  }${fade}`;
+  const bgColor = (fade = 'ff') =>
+    `#${
+      (index + 1) % 4 === 2 || (index + 1) % 4 === 3 ? '7c9edf' : '708dc2'
+    }${fade}`;
   return (
     <div
-      className="flex flex-col px-3 py-10 bg-no-repeat"
+      className="flex flex-col px-3 py-10 bg-no-repeat relative group"
       style={{
         backgroundImage: `linear-gradient(to right, #ffffff00, ${bgColor(
           'aa',
@@ -23,10 +24,10 @@ const Country = ({
         backgroundSize: '50% 100%',
       }}
     >
+      <BsFillArrowUpRightCircleFill className="text-blue-800 right-2 top-2 absolute text-lg group-hover:text-white" />
       <div className="flex flex-col gap-2 self-end font-bold text-white items-end">
         <h4 className="flex flex-col items-end">
-          <span className="text-2xl">{`${country}, \n`}</span>
-          <span className="text-sm">{continent}</span>
+          <span className="text-2xl">{country}</span>
         </h4>
         <span className="text-xl">{cases.toLocaleString()}</span>
       </div>
@@ -37,7 +38,6 @@ const Country = ({
 Country.propTypes = {
   country: PropTypes.shape({
     country: PropTypes.string.isRequired,
-    continent: PropTypes.string.isRequired,
     cases: PropTypes.number.isRequired,
     countryInfo: PropTypes.shape({
       flag: PropTypes.string.isRequired,
