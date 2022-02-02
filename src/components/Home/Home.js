7ae5b7ec57b31ee1fdf009f2f0ea102ee76a0b57
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-02 04:13:07 +0200

    Add CountUp component in Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,43 @@
 import { useSelector } from 'react-redux';
+import CountUp from 'react-countup';
+import { FcGlobe } from 'react-icons/fc';
 
 import Spinner from '../Spinner/Spinner';
 import CountryList from '../CountryList/CountryList';
 
 const Home = () => {
-  const { countries } = useSelector((state) => state.covid);
+  const {
+    countries,
+    totalData: { totalCases, totalDeaths },
+  } = useSelector((state) => state.covid);
 
   return (
     <>
       {!countries.length && <Spinner />}
-      {countries.length > 0 && <CountryList countries={countries} />}
+      {countries.length > 0 && (
+        <>
+          <div className="flex gap-3 items-center w-full justify-center pb-16 pt-6 bg-blue-dark rounded-t-lg">
+            <FcGlobe className="text-8xl bg-white rounded-full" />
+            <div className="flex flex-col font-bold text-2xl text-white">
+              <CountUp
+                delay={1}
+                end={totalCases}
+                separator=","
+                prefix="Total Cases: "
+                duration={3}
+              />
+              <CountUp
+                delay={1}
+                prefix="Total Deaths: "
+                end={totalDeaths}
+                separator=","
+                duration={3}
+              />
+            </div>
+          </div>
+          <CountryList countries={countries} />
+        </>
+      )}
     </>
   );
 };
